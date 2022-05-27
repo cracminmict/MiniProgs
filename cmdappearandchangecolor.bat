5c06085e8ab96aa368260c392edebe4997e3b801
dabet61 <101054404+dabet61@users.noreply.github.com>
2022-05-27 12:42:08 +0200

    Modifications bénines.

--- cmdappearandchangecolor.bat
+++ cmdappearandchangecolor.bat
@@ -9,41 +9,41 @@ prompt VIRUS DETECTED
 cls
 Repeat:
 color 09
-timeout /t 0.4
+
 color 4C
-timeout /Tt 0.4
+
 color 7F
-timeout /t 0.4
+
 color 3B
-timeout /t 0.4
+
 color 4E
-timeout /t 0.4
+
 color 5B
-timeout /t 0.4
+
 color 3C
-timeout /t 0.4
+
 color 19
-timeout /t 0.4
+
 color C
-timeout /t 0.4
+
 color B
-timeout /t 0.4
+
 color C
-timeout /t 0.4
+
 color D
-timeout /t 0.4
+
 color E
-timeout /t 0.4
+
 color F
-timeout /t 0.4
+
 color AE
-timeout /t 0.4
+
 color FC
-timeout /t 0.4
+
 color AC
-timeout /t 0.4
+
 color CA
 set /repeat repeat=%repeat+1
 pause
-if %repeat% == 10 goto Fin
+if %repeat% == 100 goto Fin
 goto Repeat
