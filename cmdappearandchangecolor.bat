32e2054bf860a39983ceadb3c747628e4e0cbc78
dabet61 <101054404+dabet61@users.noreply.github.com>
2022-05-31 15:49:53 +0200

    Modifications bénines.

--- cmdappearandchangecolor.bat
+++ cmdappearandchangecolor.bat
@@ -6,43 +6,113 @@ title Error detected
 color A
 prompt VIRUS DETECTED
 cls
-:Repeat
 color 09
-
 color 4C
-
 color 7F
-
 color 3B
-
 color 4E
-
 color 5B
-
 color 3C
-
 color 19
-
 color C
-
 color B
-
 color C
-
 color D
-
 color E
-
 color F
-
 color AE
-
 color FC
-
 color AC
-
 color CA
-
-set /a a=%repeat+1
-if %a% == 10 goto Fin
-goto Repeat
+color 09
+color 4C
+color 7F
+color 3B
+color 4E
+color 5B
+color 3C
+color 19
+color C
+color B
+color C
+color D
+color E
+color F
+color AE
+color FC
+color AC
+color CA
+color 09
+color 4C
+color 7F
+color 3B
+color 4E
+color 5B
+color 3C
+color 19
+color C
+color B
+color C
+color D
+color E
+color F
+color AE
+color FC
+color AC
+color CA
+color 09
+color 4C
+color 7F
+color 3B
+color 4E
+color 5B
+color 3C
+color 19
+color C
+color B
+color C
+color D
+color E
+color F
+color AE
+color FC
+color AC
+color CA
+color 09
+color 4C
+color 7F
+color 3B
+color 4E
+color 5B
+color 3C
+color 19
+color C
+color B
+color C
+color D
+color E
+color F
+color AE
+color FC
+color AC
+color CA
+color 09
+color 4C
+color 7F
+color 3B
+color 4E
+color 5B
+color 3C
+color 19
+color C
+color B
+color C
+color D
+color E
+color F
+color AE
+color FC
+color AC
+color CA
+start errormassage.vbs
+cls
